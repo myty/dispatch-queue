360a633fc37372426232674aef10f39f385efac5
Michael Tyson <myty@users.noreply.github.com>
2022-10-02 10:56:05 -0400

    Use deferred 3rd party modules for testing (#3)

---
+++ tests/deps.ts
@@ -0,0 +1,18 @@
+export { delay } from "https://deno.land/std@0.154.0/async/delay.ts";
+export {
+  afterEach,
+  beforeEach,
+  describe,
+  it,
+} from "https://deno.land/std@0.158.0/testing/bdd.ts";
+export {
+  assert,
+  assertEquals,
+  assertExists,
+} from "https://deno.land/std@0.158.0/testing/asserts.ts";
+export {
+  assertSpyCall,
+  spy,
+} from "https://deno.land/std@0.158.0/testing/mock.ts";
+export type { Spy } from "https://deno.land/std@0.158.0/testing/mock.ts";
+export { Deferred } from "https://deno.land/x/deferred@v1.0.1/mod.ts";
