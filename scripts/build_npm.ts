2eed0aa0e30b16ff9dfc126bf01279a5bf26ae0c
Michael Tyson <myty@users.noreply.github.com>
2022-10-04 20:58:03 -0400

    Publish to NPM (#5)

---
+++ scripts/build_npm.ts
@@ -0,0 +1,31 @@
+import { build, emptyDir } from "https://deno.land/x/dnt@0.30.0/mod.ts";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: "./npm",
+  test: false,
+  shims: {},
+  compilerOptions: {
+    lib: ["es2018", "dom"],
+  },
+  package: {
+    name: "@myty/dispatch-queue",
+    version: Deno.args[0].substring("refs/tags/v".length),
+    description:
+      "A dispatch queue with the ability to configure multiple queue processors.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/myty/dispatch-queue.git",
+    },
+    bugs: {
+      url: "https://github.com/myty/dispatch-queue/issues",
+    },
+  },
+});
+
+// post build steps
+Deno.copyFileSync("LICENSE", "npm/LICENSE");
+Deno.copyFileSync("README.md", "npm/README.md");
